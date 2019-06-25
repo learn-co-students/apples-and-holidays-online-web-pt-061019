c1156598f79977c23cf587dce1dddaf7207de720
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-25 20:09:34 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,6 +21,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  nuarray = holiday_hash[:winter].values
+  nuarray.each { |a| a << "Balloons" }
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
@@ -29,23 +31,39 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
+  nuarray = holiday_hash[:spring].values.flatten
+  nuarray<<supply
+holiday_hash[:spring][:memorial_day] = nuarray
+
   # add the second argument to the memorial day array
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash[season]
   # remember to return the updated hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
+
+ nu_array = []
+ holiday_hash.each do |key, value|
+   puts "#{key.capitalize}:"
+   value.each do |holidays, supplies|
+     nu_array = holidays.to_s.tr("_"," ").split
+     puts "  #{nu_array.collect {|s| s.capitalize}.join(" ")}: #{supplies.join(", ")}"
+
+     end
+   end
+end
+
+
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -54,10 +72,19 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-end
+
 
 def all_holidays_with_bbq(holiday_hash)
+    nu_array = [ ]
+    holiday_hash.each do |season, holiday|
+     holiday.each do|values, supplies|
+       if supplies.include?("BBQ")
+      nu_array<<values
+      end
+    end
+  end
+  nu_array
+end
+
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
-end
