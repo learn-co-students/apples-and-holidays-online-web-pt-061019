64459652d0b64c4a6cd8c68ae9406ad223f88de1
Lantz Warrick <betalantz@gmail.com>
2019-06-25 21:24:27 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,6 +46,10 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].map {|holiday, supplies| supplies}.flatten
 end
+# def all_winter_holiday_supplies(holiday_hash)
+#   # return an array of all of the supplies that are used in the winter season
+#   holiday_hash[:winter].values.flatten
+# end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
